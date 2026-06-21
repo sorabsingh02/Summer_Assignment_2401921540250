1d3e116aad5822ac5e285fadc9c07e3b30c44a7d
Sorab Singh <sorabsingh1502@gmail.com>
2026-06-21 14:24:42 +0530

    Week 1 Day 3 - Problem 2: Squares of a Sorted Array

---
+++ Week1/Day3/problem2.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        vector<int> result(nums.size());
+        int left = 0;
+        int right = nums.size() - 1;
+        
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            if (abs(nums[left]) > abs(nums[right])) {
+                result[i] = nums[left] * nums[left];
+                left++;
+            } else {
+                result[i] = nums[right] * nums[right];
+                right--;
+            }
+        }
+        
+        return result;
+    }
+};
