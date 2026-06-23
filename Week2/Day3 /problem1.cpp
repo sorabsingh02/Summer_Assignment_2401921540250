a8d8f08bbec0e45394280398ee9d0bebe433fa5b
Sorab Singh <sorabsingh1502@gmail.com>
2026-06-23 22:01:33 +0530

    Week 2 Day 3 - Problem 1: Implement strStr

---
+++ Week2/Day3 /problem1.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        if (needle.empty()) {
+            return 0;
+        }
+        
+        if (needle.length() > haystack.length()) {
+            return -1;
+        }
+        
+        for (int i = 0; i <= haystack.length() - needle.length(); i++) {
+            bool match = true;
+            
+            for (int j = 0; j < needle.length(); j++) {
+                if (haystack[i + j] != needle[j]) {
+                    match = false;
+                    break;
+                }
+            }
+            
+            if (match) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+};
