5996cbd967434d8258f8f43921e74782baac77c3
Sorab Singh <sorabsingh1502@gmail.com>
2026-06-23 21:57:49 +0530

    Week 2 Day 1 - Problem 3: Ransom Note

---
+++ Week2/Day1/problem3.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> map;
+        
+        for (char c : magazine) {
+            map[c]++;
+        }
+        
+        for (char c : ransomNote) {
+            if (map.find(c) == map.end() || map[c] == 0) {
+                return false;
+            }
+            map[c]--;
+        }
+        
+        return true;
+    }
+};
