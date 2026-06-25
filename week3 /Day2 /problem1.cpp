396729d4acaa889572065abc700c4984cbcd738f
Sorab Singh <sorabsingh1502@gmail.com>
2026-06-25 17:50:10 +0530

    Week 3 Day 2 - Problem 1: Merge Two Sorted Lists

---
+++ week3 /Day2 /problem1.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* current = dummy;
+        
+        while (list1 && list2) {
+            if (list1->val <= list2->val) {
+                current->next = list1;
+                list1 = list1->next;
+            } else {
+                current->next = list2;
+                list2 = list2->next;
+            }
+            current = current->next;
+        }
+        
+        if (list1) {
+            current->next = list1;
+        } else {
+            current->next = list2;
+        }
+        
+        return dummy->next;
+    }
+};
