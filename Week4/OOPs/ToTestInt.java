35629d569874cd6d2c52753923fc431540cc8cbb
Sorab Singh <sorabsingh1502@gmail.com>
2026-06-29 16:48:54 +0530

    Week 4 OOPs - ToTestInt Main Class

---
+++ Week4/OOPs/ToTestInt.java
@@ -0,0 +1,9 @@
+public class ToTestInt {
+    public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic();
+        
+        System.out.println("Square of 5: " + arithmetic.square(5));
+        System.out.println("Square of 10: " + arithmetic.square(10));
+        System.out.println("Square of 7: " + arithmetic.square(7));
+    }
+}
