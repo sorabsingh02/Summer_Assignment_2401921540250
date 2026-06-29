fed2498e3adfe51d7ab18fc6aeedf474f77d6464
Sorab Singh <sorabsingh1502@gmail.com>
2026-06-29 16:53:15 +0530

    Week 4 OOPs - Point Class

---
+++ Week4/OOPs/Point.java
@@ -0,0 +1,40 @@
+public class Point {
+    private int x;
+    private int y;
+    
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+    
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public void setX(int x) {
+        this.x = x;
+    }
+    
+    public void setY(int y) {
+        this.y = y;
+    }
+    
+    public void setXY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public int getX() {
+        return x;
+    }
+    
+    public int getY() {
+        return y;
+    }
+    
+    @Override
+    public String toString() {
+        return "Point(" + x + ", " + y + ")";
+    }
+}
